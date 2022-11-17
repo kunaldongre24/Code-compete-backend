fb13e91dc685c3ec4e89aff1e39854f16fac3334
kunal dongre <dongrekunal16@gmail.com>
2022-11-17 10:55:19 +0530

    First commit

--- catalus-backend-main/Email.js
+++
@@ -1,9 +0,0 @@
-const EmailController = require("./EmailController");
-const express = require("express");
-const router = express.Router();
-
-router.post("/user", EmailController.sendUserEmail);
-
-router.post("/admin", EmailController.sendAdminEmail);
-
-module.exports = router;
--- catalus-backend-main/routes.js
+++
@@ -1,7 +0,0 @@
-const emailRoute = require("./Email");
-const express = require("express");
-const router = express.Router();
-
-router.use("/sendmail", emailRoute);
-
-module.exports = router;
