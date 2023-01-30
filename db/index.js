3a165f7539167763bd05f138004092f5c5a51edc
kunal dongre <65659848+kunaldongre24@users.noreply.github.com>
2023-01-30 20:19:52 +0530

    C

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const { FieldValue } = require("firebase-admin/firestore");
 const serviceAccount = require('./fly-bet-firebase-adminsdk-tt62u-f97f1f6de3.json');
 
 fs.initializeApp({
-    credential: fs.credential.cert(serviceAccount)
+  credential: fs.credential.cert(serviceAccount)
 });
 const db = fs.firestore();
 module.exports = { fs, db, FieldValue };
